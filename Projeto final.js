f4b65d8105f93717ac404915070a5b4a61a57bd0
MadsonGS <50049647+MadsonGS@users.noreply.github.com>
2019-05-15 18:59:32 -0300

    Update Projeto final.js

--- Projeto final.js
+++ Projeto final.js
@@ -5,6 +5,11 @@ var yi = -10;
 var xt;
 var yt;
 var tiro = false;
+var colisao = false;
+var vidas = 5;
+var pontos = 0;
+var corBranco;
+
 
 function setup() 
 {
@@ -14,12 +19,18 @@ function setup()
 
 function draw() 
 {
+  
   background(0, 0, 0);
+  textSize(20);
+  fill(135,206,235);
+  text("Vidas: "+vidas, 10, 60);
+  text("Pontos: " + pontos, 10, 40);
+  
   if(keyIsDown(65) && x>=0) //a
   {
    x-=5 
   }
-  if(keyIsDown(68) && x<=365) //d
+  if(keyIsDown(68) && x<=400) //d
   {
    x+=5 
   }
@@ -27,7 +38,7 @@ function draw()
   {
    y-=5 
   }
-  if(keyIsDown(83) && y<=465) //s
+  if(keyIsDown(83) && y<=500) //s
   {
    y+=5 
   }
@@ -63,9 +74,33 @@ function draw()
       }
     
     }
-  circle(x, y, 15, 15) //corpo do jogador
+  if(dist(xi, yi, x, y)<18)
+  {
+    if ( colisao == false) 
+    {
+      corBranco = ! corBranco;
+      yi = random(0, -20)
+      xi = random(0, 400)
+      vidas-=1
+       colisao = true;
+    }
+  }
+  else 
+  {
+    colisao = false;
+  }
+  if ( corBranco ) 
+  {
+   fill(255);
+ }
+ else 
+ {
+   fill(255,0,0);
+ }
+  
+  
+  circle(x, y, 16, 16) //corpo do jogador
   circle(xt, yt, 4, 4) //corpo do tiro
-  rect(xi, yi, 10, 10) //inimigo
+  rect(xi, yi, 20, 20) //inimigo
     
 }
-  
