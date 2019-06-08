4a91cb1cece829216e677d8f0b35d70e28af7ee3
MadsonGS <50049647+MadsonGS@users.noreply.github.com>
2019-06-08 01:00:03 -0300

    Update Projeto final.js

--- Projeto final.js
+++ Projeto final.js
@@ -1,10 +1,5 @@
 var x = 200;
 var y = 450;
-var xi = 50;
-var yi = -10;
-var xt;
-var yt;
-var tiro = false;
 var colisao = false;
 var fome = 600;
 var saude = 100
@@ -13,17 +8,46 @@ var posX = []
 var posY = []
 var tela = 0
 var aux = 0    //Auxiliar para regeneração da saúde
+var nivel = 1
 var n = 0
 
 
 
 function preload() 
 {
+  //saudavel
   maça = loadImage('apple.png');
   banana = loadImage('banana.png');
+  broc = loadImage('broc.png')
+  uva = loadImage('uva.png')
+  cenoura = loadImage('cenoura.png')
+  
+  //não saudavel
   pizza = loadImage('pizza.png');
   hotdog = loadImage('hotdog.png');
-  broc = loadImage('broc.png')
+  refri = loadImage('refri.png');
+  batata = loadImage('batata.png');
+  bebida = loadImage('bebida.png');
+  
+  //nivel 2
+  chocolate = loadImage('choc.png');
+  chocolateb = loadImage('chocb.png');
+  rosquinha = loadImage('rosq.png');
+  biscoito = loadImage('bisco.png');
+  bolo = loadImage('bolo.png');
+  chocolate2 = loadImage('choc2.png');
+  chocolate3 = loadImage('choc3.png');
+  chocolate4 = loadImage('choc4.png');
+  biscoito2 = loadImage('bisco2.png');
+  chocolate5 = loadImage('choc5.png')
+  
+  //nivel 3
+  maçav = loadImage('maçav.png');
+  melancia = loadImage('melan.png');
+  abacaxi = loadImage('abac.png');
+  beringela = loadImage('berin.png');
+  laranja = loadImage('laranja.png');
+  
 }
 
 
@@ -48,7 +72,8 @@ function draw()
     background(0)
     textSize(40)
     fill(1,206,235)
-    text("Precione ENTER", 50, 370);
+    textFont('Georgia')
+    text("Pressione ENTER", 40, 370);
      
     if (keyIsDown(ENTER) ) 
     {
@@ -64,8 +89,21 @@ function draw()
   background(0, 0, 0);
   textSize(20);
   fill(135,206,235);
+  
+  if(nivel == 1)
+  {
   text("Fome: " + fome, 10, 40);
   text("Saúde: " + saude, 10, 20)
+  }
+  if(nivel == 2)
+  {
+  text("Saúde: " + saude, 10, 20)
+  }
+  if(nivel == 3)
+  {
+  text("Fome: " + fome, 10, 40);
+  }
+  
   
   
   
@@ -97,38 +135,85 @@ function draw()
 //Comida
   
   
-for(i=0; i<10; i++)
+for(i=0; i<20; i++)
 {
 
   //Comida saudável
   
-  if (i%2 == 0) 
+  if(i < 10 && nivel == 1 || i < 25 && nivel == 3) //Quantidade de inimigos de acordo com o nivel
+  {
+  
+   posY[i] += 5
+    
+  if (i%2 == 0 && nivel == 1 || i%2 == 0 && nivel == 3) 
   {
     fill(255, 0 , 0);
     //circle(posX[i], posY[i], 20, 20)
-    if(i==2)
+  
+    //Figuras dos objetos
+    
+    if(i==4)
     {
     imageMode(CENTER)
     image(maça, posX[i], posY[i])
     }
-    if(i==4)
+    if(i==2)
     {
     imageMode(CENTER)
     image(banana, posX[i], posY[i])
     }
-    if( i == 6)
+    if(i==0)
     {
+    imageMode(CENTER)
     image(broc, posX[i], posY[i])
     }
-  }
+    if(i==6)
+    {
+    imageMode(CENTER)
+    image(uva, posX[i], posY[i])
+    }
+    if(i==8)
+    {
+    imageMode(CENTER)
+    image(cenoura, posX[i], posY[i])
+    }
     
+    if(i==10)
+    {
+    imageMode(CENTER)
+    image(maçav, posX[i], posY[i])
+    }
+    if(i==12)
+    {
+    imageMode(CENTER)
+    image(laranja, posX[i], posY[i])
+    }
+    if(i==14)
+    {
+    imageMode(CENTER)
+    image(abacaxi, posX[i], posY[i])
+    }
+    if(i==16)
+    {
+    imageMode(CENTER)
+    image(beringela, posX[i], posY[i])
+    }
+    if(i==18)
+    {
+    imageMode(CENTER)
+    image(melancia, posX[i], posY[i])
+    }
+  }
+  
 
   //Comida não saudável
 
-   if(i%2 != 0)
+   if(i%2 != 0 && nivel == 1)
     {
     fill(0,255,0);
     //circle(posX[i], posY[i], 20, 20)
+      
+      //Figuras dos objetos
       
       if(i==3)
       {
@@ -140,45 +225,128 @@ for(i=0; i<10; i++)
       imageMode(CENTER)
       image(hotdog, posX[i], posY[i])
       }
+      if(i==5)
+      {
+      imageMode(CENTER)
+      image(refri, posX[i], posY[i])
+      }
+      if(i==7)
+      {
+      imageMode(CENTER)
+      image(batata, posX[i], posY[i])
+      }
+      if(i==9)
+      {
+      imageMode(CENTER)
+      image(bebida, posX[i], posY[i])
+      }
     }
+  }
   
-    posY[i] += 5
-    
-    if(posY[i] >= 500)
+     if(i >= 10 && i < 20 && nivel == 2) //objetos do nivel 2
+    {
+      posY[i] += 5
+      fill(250)
+      //circle(posX[i], posY[i], 20, 20)
+      
+      if(i==10)
+      {
+      imageMode(CENTER)
+      image(chocolate, posX[i], posY[i])
+      }
+      if(i==11)
+      {
+      imageMode(CENTER)
+      image(chocolateb, posX[i], posY[i])
+      }
+      if(i==12)
+      {
+      imageMode(CENTER)
+      image(rosquinha, posX[i], posY[i])
+      }
+      if(i==13)
+      {
+      imageMode(CENTER)
+      image(biscoito, posX[i], posY[i])
+      }
+      if(i==14)
+      {
+      imageMode(CENTER)
+      image(bolo, posX[i], posY[i])
+      }
+      if(i==15)
+      {
+      imageMode(CENTER)
+      image(chocolate2, posX[i], posY[i])
+      }
+      if(i==16)
+      {
+      imageMode(CENTER)
+      image(chocolate3, posX[i], posY[i])
+      }
+      if(i==17)
+      {
+      imageMode(CENTER)
+      image(chocolate4, posX[i], posY[i])
+      }
+      if(i==18)
+      {
+      imageMode(CENTER)
+      image(biscoito2, posX[i], posY[i])
+      }
+       if(i==19)
+      {
+      imageMode(CENTER)
+      image(chocolate5, posX[i], posY[i])
+      }
+      
+   
+      
+  
+    }
+    if(posY[i] >= 500 && nivel == 1 && n < 100)
     {
       
-      posY[i] = random(-1000, 0)
+      posY[i] = random(-2000, 0)
+      posX[i] = random(0, 400)
+      
+    }
+   if(posY[i] >= 500 && nivel == 2)
+    {
+      
+      posY[i] = random(-100, 0)
+      posX[i] = random(0, 400)
+      
+    }
+  if(posY[i] >= 500 && nivel == 3)
+    {
+      
+      posY[i] = random(-200, 0)
       posX[i] = random(0, 400)
       
     }
   }  
 
-  
 
-  
-  //Fome
-  
-  fome--  
   
   //colisão
   
   
   
-  
-  for(i=0; i<5; i++)
+  for(i=0; i<20; i++)
   {
+
     //Quando colidir com o alvo desejado ganha pontos
     
-    if(i%2 == 0)
+    if(i%2 == 0 && i<10 && nivel == 1 || i%2 == 0 && i<10 && nivel == 3)
     {
-      if(dist(x, y, posX[i], posY[i])<=25)
-    
+      if(dist(x, y, posX[i], posY[i])<=20)
       {
-        if ( colisao == false) 
+        if (colisao == false) 
         {
           
           posX[i] = random(0, 400)
-          posY[i] = random(-1000, 0)
+          posY[i] = random(-2000, 0)
           corBranco = !corBranco;
           fome+=100
           colisao = true;
@@ -187,27 +355,27 @@ for(i=0; i<10; i++)
           {
             aux++
           }
-          
         }
-    
       }
       else 
       {
         colisao = false;
       }
     }
+       
+    
     
     //Quando colidir com o alvo não dejesado perde vida
     
-    if(i%2 != 0)
+    if(i%2 != 0 && i<10 && nivel == 1)
     {
+      
       if(dist(x, y, posX[i], posY[i])<=20)
-    
       {
-        if ( colisao == false) 
+        if (colisao == false) 
         {
           posX[i] = random(0, 400)
-          posY[i] = random(-1000, 0)
+          posY[i] = random(-2000, 0)
           corBranco = !corBranco;
           fome+=50
           saude-= 5
@@ -219,7 +387,32 @@ for(i=0; i<10; i++)
         colisao = false;
       }
     }
-  }
+    
+    //colisão no nivel 2
+    
+    if(i>=10 && i<20 && nivel == 2 || i>=10 && i<20 && nivel == 3)
+    {
+      if(dist(x, y, posX[i], posY[i])<=20)
+      {
+        if ( colisao == false) 
+        {
+          
+          posX[i] = random(0, 400)
+          posY[i] = random(-100, 0)
+          corBranco = !corBranco;
+          colisao = true;
+          saude -= 10
+          fome+=50
+        }
+    
+      }
+      else 
+      {
+        colisao = false;
+      }
+    }
+}
+
 if(aux == 5 && saude < 100)
 {
   saude+=5
@@ -244,8 +437,33 @@ else
 }
 
 
+//Fome
   
-  
+if(nivel == 1)
+{
+  fome--
+}
+if(nivel == 2)
+{
+  fome = 1000
+}
+if(nivel == 3)
+{
+  fome-= 2
+}
+   
+n++ //ponto imaginario
+    
+if(n >= 200) //nivel 2
+{
+  nivel = 2
+}
+if(n >= 800) //nivel 3
+{
+  nivel = 3
+}
+
+
 //Objetos
   
 circle(x, y, 20, 20) //corpo do jogador
@@ -265,7 +483,8 @@ if(fome <= 0 || saude <= 0)
     background(0)
     textSize(40)
     fill(1,206,235)
-    text("Você morreu", 60, 250);
+    text("Você Está Obeso!", 60, 250);
+    text("Pressione 'Enter' para tentar novamente", 60, 300);
     if (keyIsDown(ENTER) ) 
     {
       //resetando tudo
@@ -275,10 +494,10 @@ if(fome <= 0 || saude <= 0)
       saúde = 100
       x = 200
       y = 450
-      for(i=0; i<5; i++)
+      for(i=0; i<20; i++)
       {
         posX[i] = random(0, 400)
-        posY[i] = random(-200, -10)
+        posY[i] = random(-2000, 0)
       }
       
       
